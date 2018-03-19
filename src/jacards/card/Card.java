bd86376497031a6505e21a0b5803f0f72a314832
Jonathan Mariani <trokenking@gmail.com>
2018-03-19 18:17:20 +0100

    Fixed classes and methods that were wrong before

--- src/jacards/card/Card.java
+++ src/jacards/card/Card.java
@@ -1,13 +1,14 @@
 package jacards.card;
 
-public abstract class Card<Value> {
+public abstract class Card<Value> implements Valueable<Value> {
 	private Value value;
 	
 	public Card(Value newValue) {
 		value = newValue;
 	}
 
-	public Value getType() {
+	@Override
+	public Value getValue() {
 		return value;
 	}
 }
