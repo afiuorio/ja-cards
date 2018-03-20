ed6d9ee0303d74f908ad658acdda672eadb141b9
Jonathan Mariani <trokenking@gmail.com>
2018-03-20 21:33:44 +0100

    Resolve issue1 (#4)
    First implementation of a generic Card

---
+++ src/jacards/card/Card.java
@@ -0,0 +1,13 @@
+package jacards.card;
+
+public abstract class Card {
+	private CardValue value;
+	
+	public Card(CardValue newValue) {
+		value = newValue;
+	}
+
+	public CardValue getValue() {
+		return value;
+	}
+}
